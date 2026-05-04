8c9dd841ae7d9d12cc08105eebd92c96cd851f6a
abrahan <abromero15@hotmail.com>
2026-05-03 23:13:56 -0300

    pagina mantenimiento con empresa y whatsapp

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-posts = [
-    {"titulo": "Proyecto 1", "descripcion": "Mi primer proyecto con Flask"},
-    {"titulo": "Proyecto 2", "descripcion": "Aplicación web básica"}
-]
-
 @app.route("/")
 def home():
-    return render_template("index.html", posts=posts)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
